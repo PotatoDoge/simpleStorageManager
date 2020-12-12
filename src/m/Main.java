08010a60d5a8e101f194a64db74b8626a88bb30f
Bastian <bastian.prado.v@gmail.com>
2020-12-12 15:48:18 -0800

    Log In screen has been created. Media folder has been added. syle.css s been added

--- src/m/Main.java
+++ src/m/Main.java
@@ -1,7 +1,36 @@
 package m;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+
+public class Main extends Application {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    /***
+     * First window that the user sees
+     * <--- a connection is made with the DB --->
+     *
+     * @param primaryStage primaryStage
+     * @throws Exception exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try{
+            Class.forName(JDBC_DRIVER);
+            System.out.println("A CONNECTION WITH THE DATABASE HAS BEEN ESTABLISHED");
+            Parent root= FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("GUI/LogInScreen.fxml")));
+            primaryStage.setTitle("Storage Manager");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        catch (Exception e){
+            System.out.println("ERROR");
+            System.out.println(e);
+        }
     }
 }
