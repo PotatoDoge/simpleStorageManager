b525db2ccc2c9fb3569693b0274e876765979e15
Bastian <bastian.prado.v@gmail.com>
2020-12-14 21:28:02 -0800

    All main menu icons added

--- src/Controllers/MainMenuController.java
+++ src/Controllers/MainMenuController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -17,5 +18,25 @@ public class MainMenuController implements Initializable {
 
     public void initialResources(){
 
+    }
+
+    public void exitOnAction(ActionEvent actionEvent) {
+        System.out.println("EXIT BUTTON PRESSED");
+    }
+
+    public void settingsOnAction(ActionEvent actionEvent) {
+        System.out.println("SETTINGS BUTTON PRESSED");
+    }
+
+    public void accountingOnAction(ActionEvent actionEvent) {
+        System.out.println("ACCOUTING BUTTON PRESSED");
+    }
+
+    public void hresourcesOnAction(ActionEvent actionEvent) {
+        System.out.println("HHRR BUTTON PRESSED");
+    }
+
+    public void storageOnAction(ActionEvent actionEvent) {
+        System.out.println("STORAGE BUTTON PRESSED");
     }
 }
