07af7c02d1777b106d81df95219b3fc35fa0650d
Bastian <bastian.prado.v@gmail.com>
2020-12-19 22:24:08 -0800

    New users can be inserted in the DB throughout the program. Text areas and button added in the settings fxml file.

--- src/Controllers/SettingsController.java
+++ src/Controllers/SettingsController.java
@@ -6,19 +6,26 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SettingsController {
+
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField newUserTextField;
+    @FXML
+    private TextField newPasswordTextField;
+    @FXML
+    private TextField loggedPasswordTextArea;
+    @FXML
+    private TextField loggedUserTextArea;
     @FXML
     private TableColumn <Table, String> positionColumn;
     @FXML
@@ -28,10 +35,28 @@ public class SettingsController {
     @FXML
     private AnchorPane settingsPane;
 
+    static final String DB_URL = "jdbc:mysql://localhost/StorageManager?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    static final String USER = "root";
+
+    static final String PASS = "";
+
+    static Statement stmt = null;
+
+    static Connection conn = null;
+
+    static PreparedStatement pst = null;
+
     ObservableList<Table> oblist = FXCollections.observableArrayList();
 
     public void addOnAction(ActionEvent actionEvent) {
         System.out.println("ADD PRESSED");
+        hideItems();
+        newUserTextField.setVisible(true);
+        newPasswordTextField.setVisible(true);
+        loggedUserTextArea.setVisible(true);
+        loggedPasswordTextArea.setVisible(true);
+        addButton.setVisible(true);
     }
 
     public void returnOnAction(ActionEvent actionEvent) throws IOException {
@@ -39,6 +64,7 @@ public class SettingsController {
     }
 
     public void showUsersOnAction(ActionEvent actionEvent) {
+        hideItems();
         usersTable.getItems().clear();
         usersTable.setVisible(true);
         try {
@@ -63,5 +89,65 @@ public class SettingsController {
         settingsPane.getChildren().setAll(pane);
     }
 
+    public void hideItems(){
+        newUserTextField.setVisible(false);
+        newPasswordTextField.setVisible(false);
+        loggedUserTextArea.setVisible(false);
+        loggedPasswordTextArea.setVisible(false);
+        usersTable.getItems().clear();
+        usersTable.setVisible(false);
+        addButton.setVisible(false);
 
+    }
+
+    public void popUpMessage(String header, String text){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("");
+        alert.setHeaderText(header);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
+
+    /**
+     * Method that checks if the given string contains non alpha numeric values
+     * @param str string
+     * @return true if no non alpha numeric values were found
+     */
+    public boolean isAlphanumeric(String str) {
+        char[] charArray = str.toCharArray();
+        for (char c : charArray) {
+            if (!Character.isLetterOrDigit(c))
+                return false;
+        }
+        return true;
+    }
+
+
+    public void addButtonAction(ActionEvent actionEvent) throws IOException{
+        if(newUserTextField.getText().isEmpty() || newPasswordTextField.getText().isEmpty() || loggedUserTextArea.getText().isEmpty() || loggedPasswordTextArea.getText().isEmpty()){
+            popUpMessage("Fill everything","All fields must be filled to proceed");
+        }
+        else if(newUserTextField.getText().length() > 25 || newPasswordTextField.getText().length() > 25){
+            popUpMessage("Username or password too long","Neither the username nor the\npassword can exceed 25 characters");
+        }
+        else if(!isAlphanumeric(newUserTextField.getText()) || !isAlphanumeric(newPasswordTextField.getText())){
+            popUpMessage("Not a valid character","Both the user and password must only\ncontain alpha numeric values");
+        }
+        else if(!loggedUserTextArea.getText().equals(LogInScreenController.user.getName()) || !loggedPasswordTextArea.getText().equals(LogInScreenController.user.getPass())){
+            popUpMessage("Not a valid user/password","The logged username or password\nis not correct.");
+        }
+        else{
+            try{
+                conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                stmt = conn.createStatement();
+                int insertUser = stmt.executeUpdate("INSERT INTO users (user,password) VALUES('"+newUserTextField.getText()+"','"+newPasswordTextField.getText()+"')");
+                System.out.println("insert user");
+                //int insertPass = stmt.executeUpdate("INSERT INTO users (password) VALUES('"+newPasswordTextField.getText()+"')");
+                System.out.println("Inserted successfully");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
 }
